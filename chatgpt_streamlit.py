5d846bc323754e1dc12e5c51cc3a5e65578ada7f
Amenallah berrejeb <90092833+berrejebamen@users.noreply.github.com>
2023-10-23 18:09:44 +0100

    Add files via upload

---
+++ chatgpt_streamlit.py
@@ -0,0 +1,44 @@
+import streamlit as st
+import os
+
+import openai
+from langchain.chains import ConversationalRetrievalChain, RetrievalQA
+from langchain.chat_models import ChatOpenAI
+from langchain.document_loaders import DirectoryLoader, TextLoader
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.indexes import VectorstoreIndexCreator
+from langchain.indexes.vectorstore import VectorStoreIndexWrapper
+from langchain.llms import OpenAI
+from langchain.vectorstores import Chroma
+
+os.environ["OPENAI_API_KEY"] = "private_api_key"
+
+PERSIST = False
+
+if PERSIST and os.path.exists("persist"):
+    st.write("Reusing index...\n")
+    vectorstore = Chroma(persist_directory="persist", embedding_function=OpenAIEmbeddings())
+    index = VectorStoreIndexWrapper(vectorstore=vectorstore)
+else:
+    loader = TextLoader("data.txt")
+    if PERSIST:
+        index = VectorstoreIndexCreator(vectorstore_kwargs={"persist_directory":"persist"}).from_loaders([loader])
+    else:
+        index = VectorstoreIndexCreator().from_loaders([loader])
+
+chain = ConversationalRetrievalChain.from_llm(
+    llm=ChatOpenAI(model="gpt-3.5-turbo"),
+    retriever=index.vectorstore.as_retriever(search_kwargs={"k": 1}),
+)
+
+chat_history = []
+
+st.title("ADADGPT")
+
+query = st.text_input("Prompt: ")
+
+if query:
+    result = chain({"question": query, "chat_history": chat_history})
+    st.write(result['answer'])
+
+    chat_history.append((query, result['answer']))
